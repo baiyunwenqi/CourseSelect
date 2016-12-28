98ae9256818c34c9a61012cb2dc4673ef5aa5ef0
liwenqi <lwq2012hust@163.com>
2016-12-28 11:10:42 +0000

    增加课程介绍

--- db/migrate/20161223033738_add_open_attribute.rb
+++ db/migrate/20161223033738_add_open_attribute.rb
@@ -1,5 +1,6 @@
 class AddOpenAttribute < ActiveRecord::Migration
   def change
-    add_column :courses, :open,:boolean,default:false
+    add_column :courses,:open,:boolean,:default =>false
+    add_column :courses,:course_description,:string,:default =>"" #加入课程描述
   end
 end
